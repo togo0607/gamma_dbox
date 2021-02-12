d254dd0257f0eddd37f93ad53f9776b764dacd84
togo0607 <43904208+togo0607@users.noreply.github.com>
2021-02-12 18:08:10 +0900

    Code and README created

---
+++ README.md
@@ -0,0 +1,64 @@
+<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+<script type="text/x-mathjax-config">
+ MathJax.Hub.Config({
+ tex2jax: {
+ inlineMath: [['$', '$'] ],
+ displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
+ }
+ });
+</script>
+# 分布関数から比熱比$\gamma$を計算するスクリプト
+## 分布関数と$\gamma$の関係
+$v_{\parallel} > 0$領域のみ0でない値を持つ(一方向)イオンの分布関数$f(v_{\parallel})$に対し、以下の関係が成り立つ。
+$$ \int_0^{\infty}\textrm{d}v_{\parallel}\frac{f(v_{\parallel})}{v_{\parallel}^2} = \frac{n}{u_{\parallel}^2 - \gamma T_{\textrm{i}\parallel}/m_{\textrm{i}}} $$
+詳細については別紙「プラズマ音速の導出.pdf」を参照のこと。$f(v_{\parallel})$として単一の矩形分布関数では$\gamma = 3$以外の結果を出せない(伝導熱流束が0のため)ので、このスクリプトでは矩形分布関数の重ね合わせにより二温度分布を持たせることで、その成分比に応じた$\gamma$の振る舞いを調べる。
+
+## 重ね合わせ矩形分布関数と電場による運動
+下図のような矩形を重ね合わせた分布関数を作り、二温度成分を持たせる。
+
+![分布関数](img/Fig1.svg)
+
+## 計算手順
+**Specify the characteristics of $f(v_{\parallel}$)**
+
+インプットパラメータとして下記を与える。
+- 与える分布関数が0でない値を持つ下限($v_{\\textrm{c}}$ [m/s])
+- 低温成分の温度($T_1$ [eV])
+- 高温成分の温度($T_2$ [eV])
+
+**Constants**
+
+定数として下記を与える。
+- イオン質量($m = 1.672\times 10^{-27}$ [kg]) (軽水素イオンなので状況に応じて変えても良い)
+- 素電荷($e = 1.602\times 10^{-19}$ [C])
+
+**Compute some characteristic parameters of $f(v_{\parallel})$**
+
+分布関数を特徴付ける量として以下を計算する。
+- 低温成分の幅($\Delta _1 = \sqrt{\frac{12T_1e}{m}}$ [m/s])
+- 高温成分の幅($\Delta _2 = \sqrt{\frac{12T_2e}{m}}$ [m/s])
+
+**Compute the moment quantities of $f(v_{\parallel})$**
+
+分布関数のモーメント(解析的に計算済み)を高温成分の密度の比$C_{\textrm{h}} = n_{02}/(n_{01}+n_{02})$の関数として計算する。
+- 実効流速
+$$u_{\textrm{eff}} = \frac{1}{n}\int_{-\infty}^{\infty}v_{\parallel}f(v_{\parallel})\textrm{d}v_{\parallel} = v_{\textrm{c}} + (1 - C_{\textrm{h}})\frac{\Delta_1}{2} + C_{\textrm{h}}\frac{\Delta_2}{2}$$
+- 実効温度
+$$T_{\textrm{eff}} = \frac{1}{n}\int_{-\infty}^{\infty}m(v_{\parallel} - u_{\textrm{eff}})^2f(v_{\parallel})\textrm{d}v_{\parallel} = \frac{m}{e}\left[ \frac{1}{3}\left( \left( 1 - C_{\textrm{h}} \right)\Delta_1^2 + C_{\textrm{h}}\Delta_2^2 \right) - \left( u_{\textrm{eff}} - v_{\textrm{c}} \right)^2 \right]$$
+- 伝導熱流束(開発中)
+- 比熱比
+$$\gamma = \frac{m}{eT_{\textrm{eff}}}\left(u_{\textrm{eff}}^2 - \frac{n}{\int_0^{\infty}\textrm{d}v_{\parallel}\frac{f(v_{\parallel})}{v_{\parallel}^2}} \right) 
+= \frac{m}{eT_{\textrm{eff}}}\left( u_{\textrm{eff}}^2 - \frac{v_{\textrm{c}}\left( v_{\textrm{c}} + \Delta_1 \right) \left(v_{\textrm{c}}+\Delta_2 \right) }{v_{\textrm{c}} + \left( 1 - C_{\textrm{h}} \right) \Delta_2 + C_{\textrm{h}}\Delta_1} \right)$$
+
+**Draw $f(v_{\parallel})$ for some of $C_{\textrm{h}}$**
+
+$C_{\textrm{h}} = 0, 0.2, 0.4, 0.6, 0.8, 1$の場合の分布関数$f(v_{\parallel})$ (1で規格化)を取得
+
+**Plots**
+
+以下をプロットする。
+- $f(v_{\parallel})$ ($C_{\textrm{h}} = 0, 0.2, 0.4, 0.6, 0.8, 1$) ("f_v_para.svg", "f_v_para.pdf")
+- $\gamma (C_{\textrm{h}})$ ("gamma.svg", "gamma.pdf")
+- $u_{\textrm{eff}} (C_{\textrm{h}})$ ("flow.svg", "flow.pdf")
+- $T_{\textrm{eff}} (C_{\textrm{h}})$ ("temp.svg", "temp.svg")
---
+++ gamma_dbox.py
@@ -0,0 +1,112 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.ticker import ScalarFormatter
+
+"""Compute specific heat ratio (gamma) based on a superposed 
+box-shape velocity distribution function f(v_para).
+"""
+
+###Specify the characteristics of f(v_para)###
+v_c = 1.0E5   #The lower limit velocity [m/s]
+T_1 = 20.0    #The temperature of the lower-T  component [eV]
+T_2 = 200.0   #The temperature of the higher-T component [eV]
+
+###Constants###
+m = 1.672E-27   #ion mass [kg]
+e = 1.602E-19   #elementary charge [C]
+
+###Compute some characteristic parameters of f(v_para)###
+Delta_1 = np.sqrt(12.0*T_1*e/m)   #The width of lower-T  component [m/s]
+Delta_2 = np.sqrt(12.0*T_2*e/m)   #The width of higher-T component [m/s]
+#u_1 = v_c + Delta_1/2.0   #The flow velocity of lower-T  component [m/s]
+#u_2 = v_c + Delta_2/2.0   #The flow velocity of higher-T component [m/s]
+
+###Compute the moment quantities of f(v_para)###
+c_h    = np.linspace(0.0, 1.0, 1000)   #The density ratio of higher-T component
+u_eff  = v_c + (1.0-c_h)*(Delta_1/2.0) + c_h*(Delta_2/2.0)   #The effective flow velocity [m/s]
+T_eff  = ((1.0-c_h)*Delta_1**2.0+c_h*Delta_2**2.0)/3.0 - (u_eff - v_c)**2.0   #tentative
+T_eff  = T_eff*m/e   #The effective temperature [eV]
+#q_cond =
+gamma  = u_eff**2.0 - v_c*(v_c+Delta_1)*(v_c+Delta_2)/(v_c+(1.0-c_h)*Delta_2+c_h*Delta_1)   #tentative
+gamma  = gamma/(T_eff*e/m)   #specific heat ratio
+
+###Get f(v_para) for some of c_h###
+c_h_samp = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+v_para   = np.linspace(0.0, Delta_2+2.0*v_c, 1000)
+f_v_para = np.zeros((len(c_h_samp), len(v_para)))
+for iv in range(len(v_para)):
+    if(v_para[iv] >= v_c and v_para[iv] < v_c+Delta_1):
+        for ic in range(len(c_h_samp)):
+            f_v_para[ic,iv] = (1.0-c_h_samp[ic])/Delta_1+c_h_samp[ic]/Delta_2
+    elif(v_para[iv] >= v_c+Delta_1 and v_para[iv] < v_c+Delta_2):
+        for ic in range(len(c_h_samp)):
+            f_v_para[ic,iv] = c_h_samp[ic]/Delta_2
+    else:
+        f_v_para[:,iv] = 0.0
+
+###Plots###
+plt.rcParams["font.size"] = 16
+#f(v_para)
+fig, ax = plt.subplots()
+for ic in range(len(c_h_samp)):
+    ax.plot(v_para/v_c,f_v_para[ic,:],'-',label="n/n$_{\\rm{hot}}$="+str(c_h_samp[ic]))
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+ax.tick_params(which='both',axis='both', direction='in')
+ax.legend()
+ax.set(xlim=[0,(v_para/v_c).max()*1.1],
+       ylim=[0,f_v_para.max()*1.1],
+       xlabel='v$_{\\parallel}$/v$_{\\rm{c}}$',
+       ylabel='f(v$_{\\parallel}$)',
+       title='')
+plt.savefig("f_v_para.svg",bbox_inches="tight")
+plt.savefig("f_v_para.pdf",bbox_inches="tight")
+plt.show()
+
+#gamma
+fig, ax = plt.subplots()
+ax.plot(c_h,gamma,'-')
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+ax.tick_params(which='both',axis='both', direction='in')
+ax.set(xlim=[0,1],
+       ylim=[0,gamma.max()*1.1],
+       xlabel='n$_{\\rm{hot}}$/n',
+       ylabel='$\gamma$',
+       title='')
+plt.savefig("gamma.svg",bbox_inches="tight")
+plt.savefig("gamma.pdf",bbox_inches="tight")
+plt.show()
+
+#flow velocity
+fig, ax = plt.subplots()
+ax.plot(c_h,u_eff,'-')
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+ax.tick_params(which='both',axis='both', direction='in')
+ax.set(xlim=[0,1],
+       ylim=[0,u_eff.max()*1.1],
+       xlabel='n$_{\\rm{hot}}$/n',
+       ylabel='u$_{\\parallel}$ [m/s]',
+       title='')
+plt.savefig("flow.svg",bbox_inches="tight")
+plt.savefig("flow.pdf",bbox_inches="tight")
+plt.show()
+
+#temperature
+fig, ax = plt.subplots()
+ax.plot(c_h,T_eff,'-')
+ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax.ticklabel_format(style="sci",  axis="y",scilimits=(0,0))
+ax.tick_params(which='both',axis='both', direction='in')
+ax.set(xlim=[0,1],
+       ylim=[0,T_eff.max()*1.1],
+       xlabel='n$_{\\rm{hot}}$/n',
+       ylabel='T$_{\\parallel}$ [eV]',
+       title='')
+plt.savefig("temp.svg",bbox_inches="tight")
+plt.savefig("temp.pdf",bbox_inches="tight")
+plt.show()
+
